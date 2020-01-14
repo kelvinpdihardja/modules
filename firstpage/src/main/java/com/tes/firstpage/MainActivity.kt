e0f1f3269060737e1c8e9f30e5a31ce57742643b
Kelvin Prayitno <kelvin@dihardjasoftware.com>
2020-01-14 15:37:19 +0700

    no message

--- firstpage/src/main/java/com/tes/firstpage/MainActivity.kt
+++ firstpage/src/main/java/com/tes/firstpage/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.tes.firstpage
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,17 +9,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.tes.library.R.layout.activity_main)
+        setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            com.google.android.material.snackbar.Snackbar.make(
-                view,
-                "Replace with your own action",
-                com.google.android.material.snackbar.Snackbar.LENGTH_LONG
-            )
-                .setAction("Action", null).show()
-        }
     }
 
 }
